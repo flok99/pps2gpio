6f4e63660082ebb77686ba25eb9218e0ecc9206e
Folkert van Heusden <folkert@vanheusden.com>
2015-09-26 14:42:17 +0200

    fd leackage

--- pps2gpio.cpp
+++ pps2gpio.cpp
@@ -4,23 +4,32 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/time.h>
+#include <sys/timepps.h>
 #include <sys/types.h>
 
 #include "error.h"
 #include "gpio-int-test.h"
 
-#include <sys/timepps.h>
+#ifdef __GNUG__
+        #define likely(x)       __builtin_expect((x), 1)
+        #define unlikely(x)     __builtin_expect((x), 0)
+        #define __PRAGMA_PACKED__ __attribute__ ((__packed__))
+#else
+        #define likely(x) (x)
+        #define unlikely(x) (x)
+        #define __PRAGMA_PACKED__
+#endif
+
+const char *const strings[] = { "0", "1" };
 
 inline void set_value(const int fd, const int fd2, const int value)
 {
-        static char c[32];
-        int l = snprintf(c, sizeof c, "%d", value);
+        write(fd, strings[value], 1);
 
-        write(fd, c, l);
-
-	write(fd2, c, l);
+	write(fd2, strings[value], 1);
 }
 
 void usage()
@@ -82,7 +91,25 @@ int main(int argc, char *argv[0])
 	if (gpio_pps_out_pin == -1)
 		error_exit(false, "Need to select a GPIO pin (using -g)");
 
+	if (mlockall(MCL_FUTURE) == -1)
+		error_exit(true, "mlockall(MCL_FUTURE) failed");
+
 	bool first_it = true;
+
+	gpio_export(gpio_pps_out_pin);
+	gpio_set_dir(gpio_pps_out_pin, 1);
+	int gfd = gpio_fd_open(gpio_pps_out_pin), gfd2 = -1;
+
+	if (out_pin_2 != -1)
+	{
+		gpio_export(out_pin_2);
+		gpio_set_dir(out_pin_2, 1);
+		gfd2 = gpio_fd_open(out_pin_2);
+	}
+	else
+	{
+		gfd2 = open("/dev/null", O_WRONLY);
+	}
 
 	for(;;)
 	{
@@ -114,44 +141,30 @@ int main(int argc, char *argv[0])
 		if (time_pps_setparams(ph, &params) == -1)
 			error_exit(true, "time_pps_setparams failed");
 
-		gpio_export(gpio_pps_out_pin);
-		gpio_set_dir(gpio_pps_out_pin, 1);
-		int gfd = gpio_fd_open(gpio_pps_out_pin), gfd2 = -1;
-
-		if (out_pin_2 != -1)
-		{
-			gpio_export(out_pin_2);
-			gpio_set_dir(out_pin_2, 1);
-			gfd2 = gpio_fd_open(out_pin_2);
-		}
-		else
-		{
-			gfd2 = open("/dev/null", O_WRONLY);
-		}
-
 		if (first_it && fork && daemon(0, 0) == -1)
 			error_exit(true, "Failed to fork into the background");
 
 		first_it = false;
 
-		bool state = false;
+		bool state = true;
 
 		for(;;)
 		{
-			struct timespec timeout = { 3, 0 };
-			pps_info_t infobuf;
+			static struct timespec timeout = { 3, 0 };
+			static pps_info_t infobuf;
 
-			if (time_pps_fetch(ph, PPS_TSFMT_TSPEC, &infobuf, &timeout) == -1)
+			if (unlikely(time_pps_fetch(ph, PPS_TSFMT_TSPEC, &infobuf, &timeout) == -1))
 			{
 				set_value(gfd, gfd2, false);
 				printf("Failed waiting for event\n");
 				break;
 			}
 
-			state = !state;
 			set_value(gfd, gfd2, state);
 
-			struct timeval tv;
+			state = !state;
+
+			static struct timeval tv;
 			if (gettimeofday(&tv, NULL) == -1)
 				error_exit(true, "gettimeofday failed");
 
